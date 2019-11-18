cb939a0f10ef521071df40e72c3f4e9955976dec
KJeff01 <kjeff1997@gmail.com>
2019-11-18 11:47:36 -0600

    Don't repair combat units on high oil maps if we have a decent amount of units.

--- multiplay/skirmish/cobra_includes/tactics.js
+++ multiplay/skirmish/cobra_includes/tactics.js
@@ -97,9 +97,21 @@ function repairDroid(droidID, force)
 		return true; //pretend it is busy
 	}
 
+	const SAFE_EXTREME_OIL_IGNORE_NUM = 60;
+	var mapOilLev = mapOilLevel();
+
 	const FORCE_REPAIR_PERCENT = 66;
 	const EXPERIENCE_DIVISOR = 26;
 	const HEALTH_TO_REPAIR = 67 + Math.floor(droid.experience / EXPERIENCE_DIVISOR);
+
+	//Ignore repairing combat units if one super high oil map if we have X amount of units.
+	if (countDroid(DROID_ANY, me) >= SAFE_EXTREME_OIL_IGNORE_NUM && (mapOilLev === "HIGH" || mapOilLev === "NTW"))
+	{
+		if ((droid.droidType === DROID_WEAPON || droid.droidType == DROID_CYBORG) && droid.health <= FORCE_REPAIR_PERCENT)
+		{
+			return false;
+		}
+	}
 
 	if (!isDefined(force))
 	{
