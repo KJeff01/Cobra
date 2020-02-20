7411de54e7e5643b620cc4a3e0ba3a69d97d6b74
KJeff01 <kjeff1997@gmail.com>
2020-02-20 14:30:03 -0600

    Rewrite personality choice code.

--- multiplay/skirmish/cobra_includes/adaption.js
+++ multiplay/skirmish/cobra_includes/adaption.js
@@ -105,32 +105,35 @@ function myPersonality()
 	return personality;
 }
 
-//Choose personality based on map oil/ally count or technology. Called from eventStartLevel().
-function adaptToMap() {
-	var choice = "";
-	var personal;
-	var offset;
-	const ALLY_COUNT = playerAlliance(true).length - 1;
-	const MAP_OIL_LEVEL = mapOilLevel();
+//Semi-randomly choose a personality. Called from eventStartLevel().
+function adaptToMap()
+{
 	const HIGH_TECH_LEVEL = getMultiTechLevel() >= 2;
+	var personal;
 
-	if (!HIGH_TECH_LEVEL && (((maxPlayers - 1) === 1) || ((MAP_OIL_LEVEL === "LOW") && !ALLY_COUNT)))
+	//Map to allow a higher chance for a specific personality to be chosen.
+	if (HIGH_TECH_LEVEL || highOilMap())
 	{
-		personal = ["AM", "AR", "AB", "AC"];
-		choice = personal[random(personal.length)];
-	}
-	else if ((MAP_OIL_LEVEL === "MEDIUM") || ALLY_COUNT)
-	{
-		personal = ["AM", "AR", "AB", "AC", "AA", "AL"];
-		offset = (HIGH_TECH_LEVEL && (baseType !== CAMP_CLEAN)) ? 6 : 5;
-		choice = personal[random(offset)];
+		personal = [
+			"AM", "AM",
+			"AR", "AR", "AR", "AR", "AR",
+			"AB", "AB", "AB", "AB", "AB", "AB", "AB", "AB", "AB",
+			"AC", "AC", "AC", "AC", "AC", "AC", "AC", "AC", "AC",
+			"AA", "AA", "AA",
+			"AL", "AL", "AL", "AL", "AL", "AL",
+		];
 	}
 	else
 	{
-		personal = ["AC", "AB", "AA", "AL"];
-		offset = (HIGH_TECH_LEVEL && (baseType !== CAMP_CLEAN)) ? 4 : 3;
-		choice = personal[random(offset)];
+		personal = [
+			"AM", "AM", "AM",
+			"AR", "AR", "AR", "AR", "AR",
+			"AB", "AB", "AB", "AB", "AB", "AB", "AB",
+			"AC", "AC", "AC", "AC", "AC", "AC", "AC",
+			"AA", "AA",
+			"AL", "AL", "AL", "AL",
+		];
 	}
 
-	return choice;
+	return personal[random(personal.length)];
 }
