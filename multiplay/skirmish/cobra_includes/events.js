561cd93750ebf982b7d0e4ec6f75f0eb322696e2
KJeff01 <kjeff1997@gmail.com>
2020-05-07 18:48:07 -0500

    Fix compare warning

--- multiplay/skirmish/cobra_includes/events.js
+++ multiplay/skirmish/cobra_includes/events.js
@@ -227,7 +227,7 @@ function eventAttacked(victim, attacker)
 				if ((subPersonalities[personality].resPath === "offensive") || (random(100) < 33))
 				{
 					var unit = units[i];
-					if (unit !== null && attacker !== null && distBetweenTwoPoints(unit.x, unit.y, attacker.x, attacker.y) < (GROUP_SCAN_RADIUS + 4))
+					if (unit !== null && distBetweenTwoPoints(unit.x, unit.y, attacker.x, attacker.y) < (GROUP_SCAN_RADIUS + 4))
 					{
 						orderDroidObj(unit, DORDER_ATTACK, attacker);
 					}
