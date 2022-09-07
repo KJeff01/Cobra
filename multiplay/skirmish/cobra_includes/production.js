5ba91fe602bec2ba42e3c27b87e3c2e7a32dd630
KJeff01 <kjeff1997@gmail.com>
2022-09-07 13:25:44 -0500

    Spaces
    what are these doing here.

--- multiplay/skirmish/cobra_includes/production.js
+++ multiplay/skirmish/cobra_includes/production.js
@@ -193,8 +193,8 @@ function choosePersonalityWeapon(type)
 				for (let i = lasers.length - 1; i >= 0; --i)
 				{
 					let weapObj = lasers[i];
-	 				weaponList.push(weapObj.stat);
-	 			}
+					weaponList.push(weapObj.stat);
+				}
 			}
 
 			let aa = subPersonalities[personality].antiAir.weapons;
@@ -204,11 +204,11 @@ function choosePersonalityWeapon(type)
 				aa = weaponStats.AA.weapons;
 			}
 
- 			for (let i = aa.length - 1; i >= 0; --i)
+			for (let i = aa.length - 1; i >= 0; --i)
 			{
 				let weapObj = aa[i];
- 				weaponList.push(weapObj.stat);
- 			}
+				weaponList.push(weapObj.stat);
+			}
 		}
 
 		// Allow small chance for Bunker Busters if main weapons lines are rockets.
@@ -218,11 +218,11 @@ function choosePersonalityWeapon(type)
 			skip = true;
 			let bunkerBusters = weaponStats.rockets_AS.weapons;
 
- 			for (let i = bunkerBusters.length - 1; i >= 0; --i)
+			for (let i = bunkerBusters.length - 1; i >= 0; --i)
 			{
 				let weapObj = bunkerBusters[i];
- 				weaponList.push(weapObj.stat);
- 			}
+				weaponList.push(weapObj.stat);
+			}
 		}
 
 		// Maybe choose a machinegun.
@@ -233,11 +233,11 @@ function choosePersonalityWeapon(type)
 			weaponList = [];
 			let generalAntiCyborgWeapons = weaponStats.machineguns.weapons;
 
- 			for (let i = generalAntiCyborgWeapons.length - 1; i >= 0; --i)
+			for (let i = generalAntiCyborgWeapons.length - 1; i >= 0; --i)
 			{
 				let weapObj = generalAntiCyborgWeapons[i];
- 				weaponList.push(weapObj.stat);
- 			}
+				weaponList.push(weapObj.stat);
+			}
 		}
 	}
 	else if (type === "CYBORG")
